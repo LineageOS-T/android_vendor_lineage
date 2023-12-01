718d24567cb0fb56b242f9f8b4b171b734a355d0
someone5678 <someone5678@users.noreply.github.com>
2023-12-01 22:47:41 +0900

    common_mobile: Guard Face Unlock support
    * There're some devices with native face unlock support which ParanoidSense is not required
    * So let's not enforce build for weak modality face unlock
    
    Change-Id: I0f8d34bd29fa8c47384af1e178bd827a23027841
    Signed-off-by: neophyte <neophyteplayground@gmail.com>

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -82,7 +82,8 @@ PRODUCT_PACKAGES += \
     NavigationBarMode2ButtonOverlay
 
 # Face Unlock
-TARGET_FACE_UNLOCK_SUPPORTED ?= $(TARGET_SUPPORTS_64_BIT_APPS)
+ifeq ($(TARGET_SUPPORTS_64_BIT_APPS),true)
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
 
 ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
 PRODUCT_PACKAGES += \
@@ -93,6 +94,7 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
 endif
 
 # Media
