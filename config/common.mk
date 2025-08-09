0b07af59f4f57947ec3331b7b64850c86312ef78
neophyte721 <alprjkt@gmail.com>
2025-08-09 10:08:28 +0000

    lineage: Import some font

--- config/common.mk
+++ config/common.mk
@@ -232,3 +232,5 @@ include vendor/lineage/config/version.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/lineage/config/partner_gms.mk
+# Fonts
+include vendor/lineage/config/fonts.mk
