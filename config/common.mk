ef33a85aef08b81abe149444f29eb22ccc82f08b
neophyte404 <260553983+neophyte404@users.noreply.github.com>
2026-04-30 19:38:24 +0700

    lineage: make CoreGApps only

--- config/common.mk
+++ config/common.mk
@@ -5,6 +5,11 @@ PRODUCT_BRAND ?= LineageOS
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
+# Gapps
+LINEAGE_ZIP_TYPE := CoreGApps
+LINEAGE_CORE := true
+$(call inherit-product, vendor/gms/common/common-vendor.mk)
+
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=android-google
@@ -128,7 +133,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     LineageParts \
     LineageSettingsProvider \
-    LineageSetupWizard \
     Updater
 
 PRODUCT_COPY_FILES += \
